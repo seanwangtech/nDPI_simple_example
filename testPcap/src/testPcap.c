333cc8369ac8b44a3669c692b7ac79233d4aeb68
Xiao Wang <ninglvfeihong@gmail.com>
2017-09-10 05:15:22 -0700

    testPcap 3

--- testPcap/src/testPcap.c
+++ testPcap/src/testPcap.c
@@ -13,11 +13,26 @@
 int main(int argc,char* argv[]) {
     char * dev =argv[1];
     char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* handler;
     dev = pcap_lookupdev(errbuf);
     if(dev == NULL){
             fprintf(stderr,"Couldn't find default device: %s\n", errbuf);
             return(2);
     }
     printf("Device:%s\n",dev);
+
+    handler = pcap_open_live(dev,BUFSIZ,1,1000,errbuf);
+
+    if(!handler){
+    	fprintf(stderr,"Couldn't open device: %s\n",dev);
+    	return(2);
+    }
+
+    if(pcap_datalink(handler) != DLT_EN10MB){
+    	fprintf(stderr, "Device %s doesn't provide Ethernet headers - not supported\n", dev);
+    	return(2);
+    }
+
+
     return(0);
 }
