be78b646c9b2a4fff205ca55cfff9f35dc47ab00
Xiao Wang <ninglvfeihong@gmail.com>
2017-09-10 04:05:26 -0700

    testPcap 2

--- testPcap/src/testPcap.c
+++ testPcap/src/testPcap.c
@@ -12,6 +12,12 @@
 #include <pcap.h>
 int main(int argc,char* argv[]) {
     char * dev =argv[1];
-    printf("Device: %s\n",dev);
-	return 0;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    dev = pcap_lookupdev(errbuf);
+    if(dev == NULL){
+            fprintf(stderr,"Couldn't find default device: %s\n", errbuf);
+            return(2);
+    }
+    printf("Device:%s\n",dev);
+    return(0);
 }
