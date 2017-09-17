8a0d926077892060907b89a192bd4f5199d21880
Xiao Wang <ninglvfeihong@gmail.com>
2017-09-16 18:08:23 -0700

    testnDPI static linked OK

--- testnDPIUtil/src/myUtil.c
+++ testnDPIUtil/src/myUtil.c
@@ -9,17 +9,19 @@
 
 u_int32_t current_ndpi_memory, max_ndpi_memory;
 
-void set_ndpi_flow_malloc(void* (*__ndpi_flow_malloc)(size_t size)) {
-	//do nothing, indicating use below defined method
-}
-void set_ndpi_flow_free(void  (*__ndpi_flow_free)(void *ptr)) {
-	//do nothing, indicating use below defined method
-}
 
-void ndpi_flow_free(void *ptr) {
-	ndpi_free_flow((struct ndpi_flow_struct *) ptr);
-}
-void * ndpi_flow_malloc(size_t size) {
-	return ndpi_malloc(size);
-}
-
+//
+//void set_ndpi_flow_malloc(void* (*__ndpi_flow_malloc)(size_t size)) {
+//	//do nothing, indicating use below defined method
+//}
+//void set_ndpi_flow_free(void  (*__ndpi_flow_free)(void *ptr)) {
+//	//do nothing, indicating use below defined method
+//}
+//
+//void ndpi_flow_free(void *ptr) {
+//	ndpi_free_flow((struct ndpi_flow_struct *) ptr);
+//}
+//void * ndpi_flow_malloc(size_t size) {
+//	return ndpi_malloc(size);
+//}
+//
